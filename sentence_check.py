2cd810f5e2896a243a0a048400769dae79154d1a
ivy-mondal <ivy94635@fmail.com>
2025-02-10 12:52:17 +0530

    AI parts done neow UwU

---
+++ sentence_check.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+
+def check_line(ai_sentence, player_sentence):
+    api_key = open(".env", mode="r").read()
+    client = OpenAI(api_key=api_key)
+
+    messages = [
+        {"role": "user",
+         "content": f"Your task is to compare sentence 1: {ai_sentence} and sentence 2: {player_sentence}. Write correct if they both convey same meaning and write incorrect if they don't , nothing else."}
+    ]
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages
+    )
+    result = completion.choices[0].message.content
+    return result == "correct"
+
+
+print(check_line("The cat is sleeping silently", "The cat is sleeping without making any sound"))
+print(check_line("the cat is sleeping peacefully", "the cat is sleeping"))
---
+++ sentence_gen.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+
+def gib_line(topic):
+    api_key = open(".env", mode="r").read()
+    client = OpenAI(api_key=api_key)
+
+    messages = [
+        {"role": "user",
+                 "content": f" Please generate a very simple and short sentence on the given  topic: {topic}. Write only the sentence, nothing else."}
+                ]
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages
+    )
+    result = completion.choices[0].message.content
+    return result
+
+
+print(gib_line("cat"))
